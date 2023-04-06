b26ab3002d1c32dd18e67e0129579f580f58472e
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2023-04-07 01:16:44 +0530

    commit #16

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -20,6 +20,22 @@ const HomePage = () => {
                     </ol>
                 </div>
             </div>
+            <div className='card' id='cardTitle'>
+                <div className='card-body'>
+                    <h4> Features Included :</h4>
+                    <ul className='textSize'>
+                        <li>Student</li>
+                        <ol>
+                            <li>Student can take book borrow from Library</li>
+                            <li>Show list of Books taken by Student</li>
+                            <li>Register student and List of students</li>
+                        </ol>
+                        <li>Librarian</li>
+                        <li>Books</li>
+                        <li>Book Loans</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
